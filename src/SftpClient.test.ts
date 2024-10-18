e07e34c8afaec4fdcd9be2fa339e24915111947b
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-18 17:36:06 +0200

    fix simplest SftpClient connect & exit test case

---
+++ src/SftpClient.test.ts
@@ -0,0 +1,16 @@
+import { assert, assertEquals } from "@std/assert";
+import { SftpClient } from "./SftpClient.ts";
+
+Deno.test("SftpClient connect & exit", async () => {
+    const sftpClient = new SftpClient({
+        cwd: "playground",
+        host: "maya-dev",
+        uploaderName: "test_1",
+    });
+
+    const result = await sftpClient.close();
+
+    const clientOut = result?.all;
+    assert(clientOut);
+    assertEquals(clientOut, "Connected to maya-dev.\nsftp> exit ");
+});
