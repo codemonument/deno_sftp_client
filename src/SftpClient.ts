09b586aef49416f132820468b0fbc989c11bccb6
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-18 13:32:30 +0200

    add SftpClient class with ClientOptions

---
+++ src/SftpClient.ts
@@ -0,0 +1,26 @@
+/**
+ * The options for instantiating a new SftpClient.
+ */
+export type ClientOptions = {
+    /**
+     * The sftp host to connect to. This should be the alias of the host in your ~/.ssh/config file.
+     * Note: You should configure all your ssh connection details in your ~/.ssh/config file.
+     * I've set up `my-alias`, so that I can simply pass { host: 'my-alias' } to the constructor.
+     * Your configuration is correct, when you can connect to the server manually via `sftp my-alias`.
+     */
+    host: string;
+
+    /**
+     * The local working directory where the sftp cli is initialized.
+     * (You can later navigate locally or remotely with the `sftp lcd` and `sftp cd` commands.)
+     */
+    cwd: string;
+
+    /**
+     * An identifier for the uploader. This is used for logging.
+     * This class is intended to be used with multiple instances in parallel.
+     *
+     * @example "SFTP1"
+     */
+    uploaderName: string;
+};
