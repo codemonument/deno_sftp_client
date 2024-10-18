0b21308fa00e210113022c034e6cf9723c548b12
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-18 14:49:37 +0200

    improve exaple uploader name

--- example.ts
+++ example.ts
@@ -3,5 +3,5 @@ import { SftpClient } from "./main.ts";
 const sftpClient = new SftpClient({
     cwd: "playground",
     host: "maya-dev",
-    uploaderName: "Deno SftpClient Test",
+    uploaderName: "sftp_1",
 });
