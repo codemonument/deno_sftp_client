05db16bf57c38fbcdebd2b2534be1a2346b51149
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-18 18:22:16 +0200

    add example code to bun directory

--- test/bun/index.ts
+++ test/bun/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { SftpClient } from "@codemonument/sftp-client";
+
+const sftpClient = new SftpClient({
+    cwd: "playground",
+    host: "maya-dev",
+    uploaderName: "sftp_1",
+});
+
+await sftpClient.ls();
+
+await sftpClient.close();
