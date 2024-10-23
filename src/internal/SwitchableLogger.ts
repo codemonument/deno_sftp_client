c93b7d0434d9446b02e544cb25d441e27cf2a847
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-23 12:47:33 +0200

    also add error and warn categories to logger mode

--- src/internal/SwitchableLogger.ts
+++ src/internal/SwitchableLogger.ts
@@ -7,37 +7,57 @@ import type { GenericLogger } from "../../main.ts";
 export class SwitchableLogger implements GenericLogger {
     private logger: GenericLogger;
 
-    public logMode: "normal" | "verbose" | "silent";
+    public logMode: "normal" | "verbose" | "silent" | "error" | "warn";
 
     constructor(
-        logMode: "normal" | "verbose" | "silent" = "normal",
+        logMode: "normal" | "verbose" | "silent" | "error" | "warn" = "normal",
         logger: GenericLogger = console,
     ) {
         this.logMode = logMode;
         this.logger = logger;
     }
-    log(message: string, ...metadata: unknown[]): void {
-        if (this.logMode === "normal" || this.logMode === "verbose") {
-            this.logger.log(message, ...metadata);
-        }
-    }
-    info(message: string, ...metadata: unknown[]): void {
-        if (this.logMode === "normal" || this.logMode === "verbose") {
-            this.logger.info(message, ...metadata);
-        }
-    }
+
     debug(message: string, ...metadata: unknown[]): void {
         if (this.logMode === "verbose") {
             this.logger.debug(message, ...metadata);
         }
     }
+
+    log(message: string, ...metadata: unknown[]): void {
+        if (
+            this.logMode === "verbose" ||
+            this.logMode === "normal"
+        ) {
+            this.logger.log(message, ...metadata);
+        }
+    }
+
+    info(message: string, ...metadata: unknown[]): void {
+        if (
+            this.logMode === "verbose" ||
+            this.logMode === "normal"
+        ) {
+            this.logger.info(message, ...metadata);
+        }
+    }
+
     warn(message: string, ...metadata: unknown[]): void {
-        if (this.logMode === "normal" || this.logMode === "verbose") {
+        if (
+            this.logMode === "verbose" ||
+            this.logMode === "normal" ||
+            this.logMode === "warn"
+        ) {
             this.logger.warn(message, ...metadata);
         }
     }
+
     error(message: string, ...metadata: unknown[]): void {
-        if (this.logMode === "normal" || this.logMode === "verbose") {
+        if (
+            this.logMode === "verbose" ||
+            this.logMode === "normal" ||
+            this.logMode === "warn" ||
+            this.logMode === "error"
+        ) {
             this.logger.error(message, ...metadata);
         }
     }
