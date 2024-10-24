803a267f3219aa8347f52c4d815a6d12b683be74
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-24 02:03:52 +0200

    add verbose output mode in example

--- example.ts
+++ example.ts
@@ -12,21 +12,15 @@ const sftpClient = new SftpClient({
     cwd: "playground",
     host: "maya-dev",
     uploaderName: "sftp_1",
-    logMode: "unknown-and-error",
-    // logMode: "verbose",
+    // logMode: "unknown-and-error",
+    logMode: "verbose",
 });
 
 await sftpClient.ls();
+await sftpClient.cd("playground");
 
 const pwd = await sftpClient.pwd();
 exampleLog("pwd", pwd);
-try {
-    await sftpClient.cd("www/maya.internett.de/playground");
-} catch (error) {
-    exampleLog("cd failed", error);
-}
-
-// await sftpClient.mkdir("./test/test2");
 
 await sftpClient.close();
 
